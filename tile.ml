00408d119809f705a8a6b1492fb0c3bd2ec709b1
cmr284 <cmr284@cornell.edu>
2020-10-17 13:52:16 -0400

    Implemented board setup and some tile functions

--- tile.ml
+++ tile.ml
@@ -1,14 +1,30 @@
 (* insert tile code here*)
-type t = int
 
 type coord = (int * int)
 
-exception BadCoord of coord
+(* a tile should be an x, y representing lower left corner and a color*)
+type t = {
+  location : coord;
+  color : string
+}
 
-let get_x tile = failwith "unimplemented"
+let get_x tile = 
+  match tile.location with 
+  | (x,_) -> x
 
-let get_y tile = failwith "unimplemented"
+let get_y tile = 
+  match tile.location with 
+  | (_,y) -> y
 
-let fall tile = failwith "unimplemented"
+let move_to tile x y = {tile with location = (x, y)}
+
+(* TODO: right now I wrote -1 but the actual size of a tile is 50 pixels.
+   We'll probably want to abstract this out in some way so we can change the 
+   size of tiles, I just wasn't sure how to do that rn. *)
+let fall tile = move_to tile (get_x tile) (get_y tile - 1) 
+
+let move_left tile = move_to tile (get_x tile - 1) (get_y tile)
+
+let move_right tile = move_to tile (get_x tile + 1) (get_y tile)
 
 let drop tile = failwith "unimplemented"
