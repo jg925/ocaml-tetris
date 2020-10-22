552376d661ef2ead7fe1e8f2a5d6313add55de2c
Jinny Gui <jg925@cornell.edu>
2020-10-22 15:31:42 -0400

    test case bones

--- test.ml
+++ test.ml
@@ -4,3 +4,21 @@ open Tile
 open Shapes
 open Board
 (*insert tests here*)
+
+let tile_tests = 
+  [
+
+  ]
+
+let shapes_tests = 
+  [
+
+  ]
+
+let suite = 
+  "test suite for tetris" >::: List.flatten [
+    tile_tests;
+    shapes_tests;
+  ] 
+
+let _ = run_test_tt_main suite
