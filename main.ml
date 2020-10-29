74b023a3b242b9a3283c43e0493987d2e855b586
Jacob Kraizman <jk2327@cornell.edu>
2020-10-28 22:08:33 -0400

    added code to draw a tile on board, still doesnt compile

--- main.ml
+++ main.ml
@@ -1,5 +1,9 @@
+open Tile
+open Board
+open Shapes
 
-let start () = Board.setup ()
+let test_tile = Tile.create_tile 1 2 100 0 0
+let start () = Board.setup (); Board.display_tile test_tile
 
 let display_score () = ()
 
