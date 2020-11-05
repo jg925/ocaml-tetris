43b568a00416ece4890b4bb993eeeb989176a16e
Jacob Kraizman <jk2327@cornell.edu>
2020-11-04 21:21:06 -0500

    fixed spacing, added more score code

--- main.ml
+++ main.ml
@@ -2,9 +2,13 @@ let score = 0
 
 let test_tile = Tile.create_tile 1 2 100 0 0
 
-let start () = Board.setup (); Board.display_tile test_tile
-
 let display_score () = 
+  Graphics.moveto 0 0;
   Graphics.draw_string ("Score: " ^ string_of_int score); ()
+
+let start () = 
+  Board.setup (); 
+  Board.display_tile test_tile;
+  display_score (); ()
 
 let pause () = ()
