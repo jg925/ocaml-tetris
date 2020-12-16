ec7e6a70f539f5b5aa0aceb5774377b3442ecbc4
cmr284 <cmr284@cornell.edu>
2020-12-16 13:34:11 -0500

    board documentation finished

--- board.ml
+++ board.ml
@@ -1,44 +1,75 @@
-(* [scale] defines the size of the game board and the tiles *)
+(** [scale] is the length of each tile in pixels. *)
 let scale = Tile.tile_length
 
-
-let key_array = ref [|'f'; 'h'; 't'; 'g'; 'b'; 'r'|]
-let ctl_array = ref [|"Move Left"; "Move Right"; "Rotate CW"; 
-                      "Rotate CCW"; "Fall Faster"|]
-
-
-(* [left_offset] defines the width between the left side of the window and 
-   left side of the board *)
+(** [left_offset] defines the width between the left side of the window and 
+    left side of the board *)
 let left_offset = 150
 
-(* [bottom_offset] defines the height between the bottom of the window and 
-   bottom of the board *)
+(** [bottom_offset] defines the height between the bottom of the window and 
+    bottom of the board *)
 let bottom_offset = 70
 
-(* [right_offset] defines the width between the right side of the board and 
-   right side of the window *)
+(** [right_offset] defines the width between the right side of the board and 
+    right side of the window *)
 let right_offset = 200
 
 (** [top_offset] defines the height between the top of the board and the 
     top of the window *)
 let top_offset = 70
 
-(* [x_dim] is the width of the tetris board *)
+(** [x_dim] is the width of the tetris board *)
 let x_dim = Tilearray.x_dim
 
-(* [y_dim] is the height of the tetris board *)
+(** [y_dim] is the height of the tetris board *)
 let y_dim = Tilearray.y_dim
 
-(* [outline_width] is the width of the line drawing the outline of the board. 
-   In order for coordinates to work out perfectly in edge tiles 
-   [outline_width] should be an even number.*)
+(** [outline_width] is the width of the line drawing the outline of the board. 
+    In order for coordinates to work out perfectly in edge tiles 
+    [outline_width] should be an even number.*)
 let outline_width = 4
 
-(* [gridline_width] is the width of the line drawing the inside grid of 
-   the board *)
+(** [gridline_width] is the width of the line drawing the inside grid of 
+    the board *)
 let gridline_width = 1
 
 
+
+(** [lower ()] is the y coordinate in pixels of the lower boundary
+    of the game board *)
+let lower () = bottom_offset
+
+(** [upper ()] is the y coordinate in pixels of the upper boundary
+    of the game board *)
+let upper () = lower () + y_dim * scale + 
+               (y_dim - 1) * gridline_width + outline_width
+
+(** [left ()] is the x coordinate in pixels of the left boundary
+    of the game board *)
+let left () = left_offset
+
+(** [right ()] is the x coordinate in pixels of the right boundary
+    of the game board *)
+let right () = left () + x_dim * scale + 
+               (x_dim - 1) * gridline_width + outline_width
+
+(** [width ()] is the width in pixels of the game board *)
+let width () = right () + right_offset
+
+(** [height ()] is the height in pixels of the game board *)
+let height () = upper () + top_offset
+
+
+
+
+(* The array of keys to use to carry out different actions. *)
+let key_array = ref [|'f'; 'h'; 't'; 'g'; 'b'|]
+
+(* The actions carried out by each key. *)
+let ctl_array = ref [|"Move Left"; "Move Right"; "Rotate CW"; 
+                      "Rotate CCW"; "Fall Faster"|]
+
+
+(* used for printing current settings in key array *)
 let pp_array pp_elt arr =
   let pp_elts arr =
     let rec loop n acc = function
@@ -68,14 +99,14 @@ let set_settings () =
         ANSITerminal.(print_string [white] 
                         ("Choose the keys you would like for \
                           Left, Right, Rotate CCW, Rotate CW, \
-                          Fall Faster, and Restart:\n
-                        1. adwsxr\n
-                        2. jlik,r\n
-                        3. fhtgbr\n>"));
+                          Fall Faster:\n
+                        1. adwsx\n
+                        2. jlik,\n
+                        3. fhtgb\n>"));
         match read_int () with
-        | 1 -> key_array := "adwsxr" |> explode
-        | 2 -> key_array := "jlik,r" |> explode
-        | 3 -> key_array := "fhtgbr" |> explode
+        | 1 -> key_array := "adwsx" |> explode
+        | 2 -> key_array := "jlik," |> explode
+        | 3 -> key_array := "fhtgb" |> explode
         | _ -> ANSITerminal.(print_string [blue] "invalid input\n"); 
           do_key_settings (pp_array (fun x -> Char.escaped x) !key_array) ()
       end; ()
@@ -118,63 +149,10 @@ let set_settings () =
       set_colorblind_mode cb () in 
   set_colorblind_mode false ()
 
-let lower () = bottom_offset
 
-let upper () = lower () + y_dim * scale + 
-               (y_dim - 1) * gridline_width + outline_width
-
-let left () = left_offset
-
-let right () = left () + x_dim * scale + 
-               (x_dim - 1) * gridline_width + outline_width
-
-let width () = right () + right_offset
-
-let height () = upper () + top_offset
-
-
-let display_welcome_screen () = 
-  let width = width () in
-  let height = height () in 
-
-  " " ^ (string_of_int width) ^ "x" ^ (string_of_int height) 
-  |> Graphics.open_graph;
-  Graphics.set_window_title "Tetris";
-  Graphics.set_color (Graphics.rgb 0 0 0);
-
-  Graphics.moveto (width / 2 - 55) ((height * 2) / 3);
-  Graphics.draw_string "Welcome to Tetris";
-  Graphics.moveto (width / 2 - 70) ((height * 2) / 3 - 30);
-  Graphics.draw_string "Press any key to begin"
-
-
-let display_game_over_screen score high_score = 
-  let width = width () in
-  let height = height () in 
-
-  Graphics.clear_graph ();
-  Graphics.set_color (Graphics.rgb 0 0 0);
-
-  Graphics.moveto (width / 2 - 30) ((height * 2) / 3);
-  Graphics.draw_string "Game Over!";
-  Graphics.moveto (width / 2 - 90) ((height * 2) / 3 - 30);
-  Graphics.draw_string ("Press '"^ Char.escaped !key_array.(5) ^ 
-                        "' to play another round");
-  Graphics.moveto (width / 2 - 80) ((height * 2) / 3 - 60);
-  Graphics.draw_string ("Press 'q' to quit the game");
-  if score > high_score
-  then begin
-    Graphics.moveto (width / 2 - 48) ((height * 2) / 3 - 100);  
-    Graphics.draw_string "New high score!";
-    Graphics.moveto (width / 2 - 45) ((height * 2) / 3 - 130)
-  end
-  else 
-    Graphics.moveto (width / 2 - 45) ((height * 2) / 3 - 100); 
-  Graphics.draw_string ("Your score: " ^ string_of_int score)
-
-(** [setup ()] opens a Graphics window and draws the board outline for Tetris.
-    The board is 10x20 blocks where each block is a square with width and 
-    height both equal to [scale] pixels.*)
+(** [setup_board ()] opens a Graphics window and draws the board outline for 
+    Tetris. The board is [x_dim] x [y_dim] blocks where each block is a square 
+    with width and height both equal to [scale] pixels.*)
 let setup_board () = 
   (* Draws the board outline *)
   let lower = lower () in 
@@ -203,6 +181,54 @@ let setup_board () =
     Graphics.lineto right y
   done
 
+
+let refresh () = 
+  Graphics.clear_graph (); 
+  setup_board ()
+
+
+
+
+
+let display_welcome_screen () = 
+  let width = width () in
+  let height = height () in 
+
+  " " ^ (string_of_int width) ^ "x" ^ (string_of_int height) 
+  |> Graphics.open_graph;
+  Graphics.set_window_title "Tetris";
+  Graphics.set_color (Graphics.rgb 0 0 0);
+
+  Graphics.moveto (width / 2 - 55) ((height * 2) / 3);
+  Graphics.draw_string "Welcome to Tetris";
+  Graphics.moveto (width / 2 - 70) ((height * 2) / 3 - 30);
+  Graphics.draw_string "Press any key to begin"
+
+
+let display_game_over_screen score high_score = 
+  let width = width () in
+  let height = height () in 
+
+  Graphics.clear_graph ();
+  Graphics.set_color (Graphics.rgb 0 0 0);
+
+  Graphics.moveto (width / 2 - 30) ((height * 2) / 3);
+  Graphics.draw_string "Game Over!";
+  Graphics.moveto (width / 2 - 90) ((height * 2) / 3 - 30);
+  Graphics.draw_string ("Press 'r' to play another round");
+  Graphics.moveto (width / 2 - 80) ((height * 2) / 3 - 60);
+  Graphics.draw_string ("Press 'q' to quit the game");
+  if score > high_score
+  then begin
+    Graphics.moveto (width / 2 - 48) ((height * 2) / 3 - 100);  
+    Graphics.draw_string "New high score!";
+    Graphics.moveto (width / 2 - 45) ((height * 2) / 3 - 130)
+  end
+  else 
+    Graphics.moveto (width / 2 - 45) ((height * 2) / 3 - 100); 
+  Graphics.draw_string ("Your score: " ^ string_of_int score)
+
+
 let display_controls () =
   Graphics.set_color 0;
   Graphics.moveto (left_offset / 5) (y_dim * scale / 2);
@@ -214,10 +240,34 @@ let display_controls () =
   done;
   Graphics.moveto (left_offset / 5) 
     ((y_dim * scale / 2) - (Array.length !ctl_array + 1) * 20);
-  Graphics.draw_string "Press 'p' to pause";
+  Graphics.draw_string "Press ' ' to drop";
   Graphics.moveto (left_offset / 5) 
     ((y_dim * scale / 2) - (Array.length !ctl_array + 2) * 20);
-  Graphics.draw_string "Press ' ' to drop"; ()
+  Graphics.draw_string "Press 'p' to pause"
+
+
+let display_score score = 
+  Graphics.set_color (Graphics.rgb 255 255 255);
+  Graphics.fill_rect 0 bottom_offset 
+    (left_offset - outline_width) (y_dim * scale + top_offset);
+  Graphics.set_color 0;
+  Graphics.moveto (left_offset / 5) (bottom_offset + y_dim * scale);
+  Graphics.draw_string ("Score: " ^ string_of_int score)
+
+
+let display_high_scores scores = 
+  Graphics.set_color 0;
+  Graphics.moveto (left_offset / 5) (y_dim * scale);
+  Graphics.draw_string "High Score Board";
+  for i = 0 to List.length scores - 1 do 
+    Graphics.moveto (left_offset / 5) (y_dim * scale - (i + 1) * 20);
+    Graphics.draw_string (string_of_int (i + 1) ^ ". " 
+                          ^ string_of_int (List.nth scores i))
+  done
+
+
+
+
 
 (* functions for displaying different assets of the game *)
 
@@ -258,18 +308,25 @@ let draw_square bevel color tile_x tile_y =
                          (inner_x, inner_y + inner_scale)|]
   else ()
 
+
 let display_tile tile bevel color = 
   let x = Tile.get_x tile in
   let y = Tile.get_y tile in
   draw_square bevel color x y 
 
+
 let erase_coords x y = 
   draw_square false (Graphics.rgb 255 255 255) x y 
+
 
 let erase_tile tile = 
   let x = Tile.get_x tile in
   let y = Tile.get_y tile in
   erase_coords x y
+
+
+
+
 
 let rec display_each_tile shadow = function
   | [] -> ()
@@ -279,6 +336,32 @@ let rec display_each_tile shadow = function
     else display_tile tile true (Tile.get_color tile); 
     display_each_tile shadow t
 
+let rec erase_each_tile = function
+  | [] -> ()
+  | tile::t -> erase_tile tile; erase_each_tile t
+
+let display_shape shape = 
+  shape 
+  |> Shapes.get_tiles 
+  |> display_each_tile false
+
+let display_shadow shape = 
+  shape 
+  |> Shapes.get_tiles 
+  |> display_each_tile true
+
+let erase_shape shape = 
+  shape 
+  |> Shapes.get_tiles 
+  |> erase_each_tile
+
+
+
+
+let display_next_shape_words () = 
+  Graphics.set_color 0;
+  Graphics.moveto (right () + scale) (lower () + y_dim * scale);
+  Graphics.draw_string "The next shape will be:"
 
 let rec shift_tiles_off_board acc = function
   | [] -> acc
@@ -294,59 +377,20 @@ let display_next_shape shape =
   |> shift_tiles_off_board []
   |> display_each_tile false
 
-
-
-let display_shape shape = 
-  shape 
-  |> Shapes.get_tiles 
-  |> display_each_tile false
-
-let display_shadow shape = 
-  shape 
-  |> Shapes.get_tiles 
-  |> display_each_tile true
-
-let rec erase_each_tile = function
-  | [] -> ()
-  | tile::t -> erase_tile tile; erase_each_tile t
-
-
 let erase_last_next_shape shape = 
   shape
   |> Shapes.get_tiles
   |> shift_tiles_off_board []
   |> erase_each_tile
 
-let erase_shape shape = 
-  shape 
-  |> Shapes.get_tiles 
-  |> erase_each_tile
 
 
 
-let display_score score = 
-  Graphics.set_color (Graphics.rgb 255 255 255);
-  Graphics.fill_rect 0 bottom_offset 
-    (left_offset - outline_width) (y_dim * scale + top_offset);
-  Graphics.set_color 0;
-  Graphics.moveto (left_offset / 5) (bottom_offset + y_dim * scale);
-  Graphics.draw_string ("Score: " ^ string_of_int score)
 
 
-let display_high_scores scores = 
-  Graphics.set_color 0;
-  Graphics.moveto (left_offset / 5) (y_dim * scale);
-  Graphics.draw_string "High Score Board";
-  for i = 0 to List.length scores - 1 do 
-    Graphics.moveto (left_offset / 5) (y_dim * scale - (i + 1) * 20);
-    Graphics.draw_string (string_of_int (i + 1) ^ ". " 
-                          ^ string_of_int (List.nth scores i))
-  done
 
-let display_next_shape_words () = 
-  Graphics.set_color 0;
-  Graphics.moveto (right () + scale) (lower () + y_dim * scale);
-  Graphics.draw_string "The next shape will be:"
+
+
 
 
 let check_if_fallen shape =
@@ -384,6 +428,3 @@ let check_rows board =
   !rows
 
 
-let refresh () = 
-  Graphics.clear_graph (); 
-  setup_board ()
--- board.mli
+++ board.mli
@@ -1,17 +1,29 @@
+(** [key_array] stores the keys that will be used to move the shape Left, 
+    Right, Rotate CCW, Rotate CW, and Fall Faster in that order. *)
+val key_array : char array ref
 
 (** [set_settings ()] takes user input for game settings and sets the 
     corresponding refs to the chosen settings.
     Keybind settings:
-    adwsxr
-    jlik,r
-    fhtgbr
+        adwsxr
+        jlik,r
+        fhtgbr
     Color palette settings:
-    Regular
-    Deuteranopia
-    Protanopia
-    Tritanopia
-    Monochromacy *)
+        Regular
+        Deuteranopia
+        Protanopia
+        Tritanopia
+        Monochromacy *)
 val set_settings : unit -> unit
+
+(** [setup_board ()] creates a Tetris game board including grid lines upon 
+    which Tetris pieces can be drawn. *)
+val setup_board : unit -> unit
+
+(** [refresh ()] clears the game board and draws a new game board. *)
+val refresh : unit -> unit
+
+
 
 (** [display_welcome_screen ()] displays a welcome message for the user 
     prompting them to press any key to begin. *)
@@ -23,55 +35,61 @@ val display_welcome_screen : unit -> unit
     whether it was a new high score. *)
 val display_game_over_screen : int -> int -> unit
 
-(** [setup_board ()] creates a Tetris game board including grid lines upon 
-    which Tetris pieces can be drawn. *)
-val setup_board : unit -> unit
-
-
-val key_array : char array ref
-
+(** [display_controls ()] displays the keys the user needs to press to move
+    the shape Left, Right Rotate CCW, Rotate CW, Fall Faster, Drop, and 
+    pause the game. *)
 val display_controls : unit -> unit
 
-val left : unit -> int
+(** [display_score score] displays the score [score] on the game board. *)
+val display_score : int -> unit
 
-val right : unit -> int
+(** [display_high_scores high_score] displays the high score board 
+    [high_scores] on the board. *)
+val display_high_scores : int list -> unit
 
-val upper : unit -> int
-(** 
 
-   NOTE: need to update this spec
 
-   [display_tile tile] draws [tile] on the game board. *)
+(** [display_tile tile bevel color] draws a tile on the game board at the 
+    location contained in [tile] using the color [color]. If [bevel] is
+    true the tile is drawn with beveled edges, otherwise it is drawn as
+    one solid color. *)
 val display_tile : Tile.t -> bool -> Graphics.color -> unit
 
-
-val display_next_shape : Shapes.t -> unit
-
-
-(** [display_shape shape] draws [shape] on the game board. *)
-val display_shape : Shapes.t -> unit
-
-val display_shadow : Shapes.t -> unit
-
-(** [erase_tile tile] erases [tile] from the game board. *)
+(** [erase_tile tile] erases [tile] from the game board by drawing a white 
+    tile at the location contained in [tile]. *)
 val erase_tile : Tile.t -> unit
 
-
+(** [erase_coords x y] draws a white tile at the location specified by [x]
+    and [y]. *)
 val erase_coords : int -> int -> unit
 
 
-val erase_last_next_shape : Shapes.t -> unit
 
-(** [erase_shape shape] erases [shape] from the game board. *)
+(** [display_shape shape] draws [shape] on the game board at the locations 
+    specified by the tiles in [shape]. *)
+val display_shape : Shapes.t -> unit
+
+(** [display_shadow shape] draws [shape] on the game board at the locations 
+    specified by the tiles in [shape] with a light gray color. *)
+val display_shadow : Shapes.t -> unit
+
+(** [erase_shape shape] erases [shape] from the game board by drawing white 
+    squares at the location of each of the tiles contained in [shape]. *)
 val erase_shape : Shapes.t -> unit
 
-(** [display_score score] draws the score [score] on the game board. *)
-val display_score : int -> unit
 
 
-val display_high_scores : int list -> unit
-
+(** [display_next_shape_words ()] displays words above where the next shape 
+    the user will have is drawn. *)
 val display_next_shape_words : unit -> unit
 
-(** [refresh ()] clears the game board and draws a new game board. *)
-val refresh : unit -> unit
+(** [display_next_shape shape] displays [shape] off to the side of the game 
+    board. This function is intended to be used for showing the user what
+    shape they will have to play next. *)
+val display_next_shape : Shapes.t -> unit
+
+(** [erase_last_next_shape shape] erases the [shape] displayed to the side
+    of the board by drawing white squares at the location of each of it's 
+    tiles *)
+val erase_last_next_shape : Shapes.t -> unit
+
